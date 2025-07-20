49afb5406d1e379eccba5f8cf3c6f506abf29bf6
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-21 01:51:06 +0700

    chore(seeds): update user and profile seed data structure

--- src/seeders/20250719190925-demo-users.js
+++ src/seeders/20250719190925-demo-users.js
@@ -4,35 +4,35 @@ const { hashPassword } = require("../utils/password"); // sesuaikan path-nya
 
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.bulkInsert("Users", [
+    await queryInterface.bulkInsert("users", [
       {
         email: "admin@mail.com",
         password: await hashPassword("admin123"),
-        roles: "admin",
+        role: "admin",
         profile_id: 1,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         email: "yusuf@mail.com",
         password: await hashPassword("yusuf123"),
-        roles: "user",
+        role: "user",
         profile_id: 2,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
       },
       {
         email: "budi@mail.com",
         password: await hashPassword("budi123"),
-        roles: "user",
+        role: "user",
         profile_id: 3,
-        createdAt: new Date(),
-        updatedAt: new Date(),
+        created_at: new Date(),
+        updated_at: new Date(),
       },
     ]);
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.bulkDelete("Users", null, {});
+    await queryInterface.bulkDelete("users", null, {});
   },
 };
