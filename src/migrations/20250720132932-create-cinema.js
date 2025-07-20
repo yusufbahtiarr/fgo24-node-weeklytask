c3223f915826c89acccb66034bf01476f84193df
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-20 20:34:33 +0700

    refactor: change createdAt and updatedAt to created_at and updated_at in Cinema migration

--- src/migrations/20250720132932-create-cinema.js
+++ src/migrations/20250720132932-create-cinema.js
@@ -17,11 +17,11 @@ module.exports = {
         type: Sequelize.STRING,
         allowNull: false,
       },
-      createdAt: {
+      created_at: {
         allowNull: false,
         type: Sequelize.DATE,
       },
-      updatedAt: {
+      updated_at: {
         allowNull: false,
         type: Sequelize.DATE,
       },
