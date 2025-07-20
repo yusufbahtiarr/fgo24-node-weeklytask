f24f5c63cbe526d5870c6a41eccf1ed31f6b0bd7
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-20 21:41:21 +0700

    chore(migration): rename table from "PaymentMethods" to "payment_methods"

--- src/migrations/20250720135127-create-payment-method.js
+++ src/migrations/20250720135127-create-payment-method.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("PaymentMethods", {
+    await queryInterface.createTable("payment_methods", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -28,6 +28,6 @@ module.exports = {
     });
   },
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("PaymentMethods");
+    await queryInterface.dropTable("payment_methods");
   },
 };
