1ee958e5f9ce07e88a262f547e45baad87a51753
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-20 20:28:02 +0700

    feat: add Director model and migration

---
+++ src/migrations/20250720132501-create-director.js
@@ -0,0 +1,29 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("directors", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      director_name: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("directors");
+  },
+};
