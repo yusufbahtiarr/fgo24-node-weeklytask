4820b7674044d79eb658a88afc25862d26017f6d
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-21 23:33:13 +0700

    refactor(migration): rename movies_casts to movie_casts and adjust schema

--- src/migrations/20250720141608-create-movie-cast.js
+++ src/migrations/20250720141608-create-movie-cast.js
@@ -2,7 +2,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.createTable("movies_casts", {
+    await queryInterface.createTable("movie_casts", {
       id: {
         allowNull: false,
         autoIncrement: true,
@@ -43,6 +43,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.dropTable("movies_casts");
+    await queryInterface.dropTable("movie_casts");
   },
 };
