b0166c740d9f98fac905769669c9825dc0d6787b
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-21 09:53:41 +0700

    feat(movies): add get movie by id endpoint

--- src/routers/movies.router.js
+++ src/routers/movies.router.js
@@ -2,5 +2,6 @@ const moviesRouter = require("express").Router();
 const moviesController = require("../controllers/movies.controller");
 
 moviesRouter.get("", moviesController.getAllMovies);
+moviesRouter.get("/:id", moviesController.getMovieByID);
 
 module.exports = moviesRouter;
