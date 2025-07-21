0a7fa4c608a4e6c8c0cc0a39e24cc23b408db2d6
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-21 10:04:48 +0700

    feat(movies): add get upcoming movie endpoint

--- src/routers/movies.router.js
+++ src/routers/movies.router.js
@@ -2,6 +2,7 @@ const moviesRouter = require("express").Router();
 const moviesController = require("../controllers/movies.controller");
 
 moviesRouter.get("", moviesController.getAllMovies);
+moviesRouter.get("/upcoming", moviesController.getUpcomingMovies);
 moviesRouter.get("/:id", moviesController.getMovieByID);
 
 module.exports = moviesRouter;
