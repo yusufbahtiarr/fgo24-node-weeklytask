8c80c952ea13b469d110479ee43e4affc122a2a1
yusufbahtiarr <yusufbdev@gmail.com>
2025-07-21 10:51:27 +0700

    feat(admin): add GET /admin/movies/:id endpoint to retrieve movie by id

--- src/controllers/admin.controller.js
+++ src/controllers/admin.controller.js
@@ -78,3 +78,84 @@ exports.getAllMovies = async function (_req, res) {
     });
   }
 };
+
+/**
+ * @param {import("express").Request} req
+ * @param {import("express").Response} res
+ */
+
+exports.getMovieByID = async function (req, res) {
+  try {
+    const { id } = req.params;
+    const movie = await Movie.findOne({
+      where: { id: parseInt(id) },
+      attributes: { exclude: ["createdAt", "updatedAt"] },
+      include: [
+        {
+          model: Cast,
+          as: "casts",
+          attributes: ["cast_name"],
+          through: { attributes: [] },
+        },
+        {
+          model: Genre,
+          as: "genres",
+          attributes: ["genre_name"],
+          through: { attributes: [] },
+        },
+        {
+          model: Director,
+          as: "directors",
+          attributes: ["director_name"],
+          through: { attributes: [] },
+        },
+      ],
+    });
+
+    if (!movie) {
+      return res.status(http.HTTP_STATUS_NOT_FOUND).json({
+        success: false,
+        message: "Movie tidak ditemukan",
+      });
+    }
+
+    const result = {
+      id: movie.id,
+      poster_url: movie.poster_url,
+      backdrop_url: movie.backdrop_url,
+      title: movie.title,
+      release_date: movie.release_date,
+      runtime: movie.runtime,
+      overview: movie.overview,
+      casts:
+        movie.casts.length > 0
+          ? movie.casts.map((cast) => cast.cast_name || "Unknown").join(", ")
+          : "No casts",
+      genres:
+        movie.genres.length > 0
+          ? movie.genres
+              .map((genre) => genre.genre_name || "Unknown")
+              .join(", ")
+          : "No genres",
+      directors:
+        movie.directors.length > 0
+          ? movie.directors
+              .map((director) => director.director_name || "Unknown")
+              .join(", ")
+          : "No directors",
+    };
+
+    res.status(http.HTTP_STATUS_OK).json({
+      success: true,
+      message: "Data movie berhasil diambil",
+      data: result,
+    });
+  } catch (error) {
+    console.error("Error in getMovieByID:", error);
+    return res.status(http.HTTP_STATUS_INTERNAL_SERVER_ERROR).json({
+      success: false,
+      message: "Terjadi kesalahan server saat mengambil data movie",
+      error: error.message,
+    });
+  }
+};
--- src/routers/admin.router.js
+++ src/routers/admin.router.js
@@ -4,5 +4,11 @@ const { verifyToken } = require("../middlewares/auth.middleware");
 const { isAdmin } = require("../middlewares/role.middleware");
 
 adminRouter.get("/movies", verifyToken, isAdmin, adminController.getAllMovies);
+adminRouter.get(
+  "/movies/:id",
+  verifyToken,
+  isAdmin,
+  adminController.getMovieByID
+);
 
 module.exports = adminRouter;
